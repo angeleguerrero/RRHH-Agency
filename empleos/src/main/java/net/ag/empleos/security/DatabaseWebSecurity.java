c2fd9ad9988edf095d3c7653577d510f6d59f8e8
HELLAS <HELLAS@HELLAS>
2020-07-01 22:38:36 -0400

    Controlador Usuario con encriptado Encriptar password de registro usuarios

--- empleos/src/main/java/net/ag/empleos/security/DatabaseWebSecurity.java
+++ empleos/src/main/java/net/ag/empleos/security/DatabaseWebSecurity.java
@@ -43,6 +43,7 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter{
 		.antMatchers(
 				"/",
 				"/singup",
+				"/registrarse",
 				"/buscarvacante",
 				"/viewe"
 				).permitAll()
